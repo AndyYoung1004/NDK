ca0163769052b4178189e1e53f5bfe3beadf58b1
yangliu <yangliu@oppo.com>
2023-02-11 00:44:57 +0800

    ndk mediacodec demo

---
+++ testndk/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,99 @@
+#include <jni.h>
+#include <string>
+#include <media/NdkMediaCodec.h>
+#include <media/NdkMediaExtractor.h>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
+#include <android/log.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, "keymatch", __VA_ARGS__)
+
+AMediaExtractor* mediaExtractor = NULL;
+AMediaCodec* mediaCodec = NULL;
+
+std::string jstringTostring(JNIEnv* env, jstring jstr) {
+    char *rtn = NULL;
+    jclass clsstring = env->FindClass("java/lang/String");
+    jstring strencode = env->NewStringUTF("GB2312");
+    jmethodID mid = env->GetMethodID(clsstring, "getBytes", "(Ljava/lang/String;)[B");
+    jbyteArray barr = (jbyteArray) env->CallObjectMethod(jstr, mid, strencode);
+    jsize alen = env->GetArrayLength(barr);
+    jbyte *ba = env->GetByteArrayElements(barr, JNI_FALSE);
+    if (alen > 0) {
+        rtn = (char *) malloc(alen + 1);
+        memcpy(rtn, ba, alen);
+        rtn[alen] = 0;
+    }
+    env->ReleaseByteArrayElements(barr, ba, 0);
+    std::string stemp(rtn);
+    free(rtn);
+    return stemp;
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_testndk_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
+
+void* playThread(void* args) {
+    int inputIndex;
+    for(;;) {
+        inputIndex = AMediaCodec_dequeueInputBuffer(mediaCodec, 10000);
+        if (inputIndex >= 0) {
+            size_t bufSize;
+            auto buf = AMediaCodec_getInputBuffer(mediaCodec, inputIndex, &bufSize);
+            auto sampleSize = AMediaExtractor_readSampleData(mediaExtractor, buf, bufSize);
+            if (sampleSize < 0) {
+                break;
+            }
+            auto pts = AMediaExtractor_getSampleTime(mediaExtractor);
+            AMediaCodec_queueInputBuffer(mediaCodec, inputIndex, 0, sampleSize, pts, 0);
+            AMediaExtractor_advance(mediaExtractor);
+        }
+        AMediaCodecBufferInfo info;
+        auto status = AMediaCodec_dequeueOutputBuffer(mediaCodec, &info, 0);
+        if (status >= 0) {
+            LOGD("yangliu pts:%d",info.presentationTimeUs/1000);
+            AMediaCodec_releaseOutputBuffer(mediaCodec, status, info.size != 0);
+            usleep(30);
+        }
+    }
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_example_testndk_MainActivity_playVideo(
+        JNIEnv* env,
+        jobject /* this */, jstring path, jobject surface) {
+    const char *filePath = jstringTostring(env, path).c_str();
+    mediaExtractor = AMediaExtractor_new();
+    ANativeWindow* aNativeWindow = ANativeWindow_fromSurface(env, surface);
+    pthread_t thread;
+    media_status_t err = AMediaExtractor_setDataSource(mediaExtractor, filePath);
+    if (err != AMEDIA_OK) {
+        LOGD("set data source err");
+        return;
+    }
+    int numTracks = AMediaExtractor_getTrackCount(mediaExtractor);
+    for (int i = 0; i < numTracks; i++) {
+        AMediaFormat *format = AMediaExtractor_getTrackFormat(mediaExtractor, i);
+        const char *s = AMediaFormat_toString(format);
+        LOGD("HWCodecPlayer::InitDecoder track %d format: %s", i, s);
+        const char *mime;
+        if (!AMediaFormat_getString(format, AMEDIAFORMAT_KEY_MIME, &mime)) {
+            LOGD("HWCodecPlayer::InitDecoder no mime type");
+            return;
+        } else if (!strncmp(mime, "video/", 6)) {
+            AMediaExtractor_selectTrack(mediaExtractor, i);
+            mediaCodec = AMediaCodec_createDecoderByType(mime);
+            AMediaCodec_configure(mediaCodec, format, aNativeWindow, NULL, 0);
+            AMediaCodec_start(mediaCodec);
+        }
+        AMediaFormat_delete(format);
+    }
+    pthread_create(&thread, NULL, playThread, NULL);
+}
---
+++ testndk/app/src/main/java/com/example/testndk/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.testndk;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.example.testndk.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private SurfaceView surfaceView;
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermission();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        surfaceView = findViewById(R.id.surfaceView);
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                playVideo("/sdcard/DCIM/HEVC.mp4", surfaceHolder.getSurface());
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+            }
+        });
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    public native void playVideo(String path, Surface surface);
+
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PermissionChecker.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PermissionChecker.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.CAMERA}, 0);
+            return false;
+        }
+        return true;
+    }
+}
